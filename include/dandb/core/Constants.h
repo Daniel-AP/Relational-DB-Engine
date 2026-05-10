8fe9fdfac3554a2b572e4a676614cbf80e923b92
Daniel-AP <daniel24.ap@gmail.com>
2026-05-10 15:34:00 -0600

    feat: add shared constants

---
+++ include/dandb/core/Constants.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+
+namespace dandb {
+    namespace core {
+
+        using PageId = std::int32_t;
+        using SlotId = std::int16_t;
+
+        inline constexpr std::uint32_t PAGE_SIZE_BYTES = 4096;
+        inline constexpr PageId INVALID_PAGE_ID = 0xFFFFFFFF;
+        inline constexpr SlotId INVALID_SLOT_ID = 0xFFFF;
+        inline constexpr PageId FIRST_ALLOCATABLE_PAGE_ID = 1;
+
+    }
+}
