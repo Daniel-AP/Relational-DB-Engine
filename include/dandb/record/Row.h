129c615e098e6a845e614d0b17b77cc12257b3a6
Daniel-AP <daniel24.ap@gmail.com>
2026-05-13 15:54:51 -0600

    feat(Row): create Row class

---
+++ include/dandb/record/Row.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <dandb/record/Value.h>
+
+#include <cstddef>
+#include <utility>
+#include <vector>
+
+namespace dandb {
+    namespace record {
+
+        class Row {
+            public:
+                explicit Row(std::vector<Value> values)
+                    : values_(std::move(values)) {
+
+                }
+
+                std::size_t valueCount() const {
+                    return values_.size();
+                }
+
+                const Value& value(std::size_t index) const {
+                    return values_[index];
+                }
+
+                const std::vector<Value>& values() const {
+                    return values_;
+                }
+
+            private:
+                std::vector<Value> values_;
+        };
+
+    }
+}
